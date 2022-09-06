1e10b6c8720ff8d79e7e4d83ba811e078b020217
pablofontanessi <pablofontanessi@gmail.com>
2022-09-06 17:21:30 -0300

    importacion que rompe registro resuelta

--- englishwebsite/englishwebsite_app/forms.py
+++ englishwebsite/englishwebsite_app/forms.py
@@ -4,7 +4,7 @@ from django import forms
 from django.forms import ModelForm, ModelChoiceField
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Post, User, Exercise_Category
+from .models import Post, Exercise_Category
 
 
 

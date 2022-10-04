26b368ed39452035a38561e57c6ca3637374f58c
Juan Carignano <69175258+juancarignano@users.noreply.github.com>
2022-10-03 22:23:56 -0300

    Comment update

--- englishwebsite/englishwebsite_app/models.py
+++ englishwebsite/englishwebsite_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Exercise_Category(models.Model):
@@ -50,6 +51,7 @@ class Professor(models.Model):
 
 class Comment(models.Model):
     professor = models.ForeignKey(Professor, on_delete=models.CASCADE, default=1)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     name = models.CharField(max_length=50)
     body = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
